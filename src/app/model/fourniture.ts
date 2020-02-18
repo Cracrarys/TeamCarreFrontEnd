ff786905579193309f21587821d3cdae4acc9cdd
TruongGiang <t.nguyenpham@icloud.com>
2020-02-18 10:37:56 +0100

    jeanBranche

--- src/app/model/fourniture.ts
+++ src/app/model/fourniture.ts
@@ -1,2 +1,14 @@
+import { FormulaireEmprunt } from './formulaire-emprunt';
+
+import { Employe } from './employe';
+
 export class Fourniture {
+    idFourniture: number;
+    nomFourniture: String;
+    quantiteDisponible: number;
+    quantiteTotale: number;
+    typeFourniture: String;
+    consommable: boolean;
+    formulaire: FormulaireEmprunt[];
+    employe: Employe;
 }
