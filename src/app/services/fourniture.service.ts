53bed03cac13d89dcc67619a6f097bf8ecc825d2
TruongGiang <t.nguyenpham@icloud.com>
2020-02-18 12:04:59 +0100

    jeanBranche2+service

--- src/app/services/fourniture.service.ts
+++ src/app/services/fourniture.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Fourniture } from '../model/fourniture';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FournitureService {
-
-  constructor() { }
+  public Fourniture_API = 'http://localhost:8080' + '/fournitureRestController';
+  constructor(private http: HttpClient) { }
+  ajoutFourniture(four: Fourniture): Observable<any> {
+    return this.http.post(this.Fourniture_API + '/ajout', four);
+  };
+  suppFourniture(id: string): Observable<any> {
+    return this.http.delete(this.Fourniture_API + '/supprimer/' + id);
+  };
+  findAllFourniture(): Observable<any> {
+    return this.http.get(this.Fourniture_API + '/getAll');
+  }
+  findFourniture(id: string): Observable<any> {
+    return this.http.get(this.Fourniture_API + '/getbyid/' + id);
+  }
 }
