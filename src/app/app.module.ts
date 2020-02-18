10caa4bd2e8bb465a582bc1de0c8cf1e56314900
Cracrarys <sim.lobrot@gmail.com>
2020-02-18 10:17:18 +0100

    app/app.module: poepoe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeComponent } from './employe/employe.component';
-import { DocumentRHComponent } from './document-rh/document-rh.component';
-import { FournitureComponent } from './fourniture/fourniture.component';
-import { FormulaireEmpruntComponent } from './formulaire-emprunt/formulaire-emprunt.component';
+import { EmployeComponent } from './entity/employe/employe.component';
+import { DocumentRHComponent } from './entity/document-rh/document-rh.component';
+import { FournitureComponent } from './entity/fourniture/fourniture.component';
+import { FormulaireEmpruntComponent } from './entity/formulaire-emprunt/formulaire-emprunt.component';
 
 @NgModule({
   declarations: [
