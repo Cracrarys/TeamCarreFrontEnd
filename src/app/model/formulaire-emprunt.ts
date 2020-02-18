6364eacab1a314fe162cffc884e3b9c892a9326a
Cracrarys <sim.lobrot@gmail.com>
2020-02-18 10:57:08 +0100

    blabla

--- src/app/model/formulaire-emprunt.ts
+++ src/app/model/formulaire-emprunt.ts
@@ -1,7 +1,14 @@
+import { Employe } from './employe';
+import { Fourniture } from './fourniture';
+
 export class FormulaireEmprunt {
     idFormulaire:number;
     typeFormulaire:string;
+    quantite:number;
     nomFormulaire:string;
     dateEmprunt:Date;
     dateRetour:Date;
+    employe:Employe;
+    demandeValidee:Boolean;
+    fourniture:Fourniture;
 }
