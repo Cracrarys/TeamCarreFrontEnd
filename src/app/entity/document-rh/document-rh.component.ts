4757607d6b88d649e15f523f0cb2b805b7704d51
Cracrarys <sim.lobrot@gmail.com>
2020-02-18 17:04:22 +0100

    lever du jour

--- src/app/entity/document-rh/document-rh.component.ts
+++ src/app/entity/document-rh/document-rh.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DocumentRH } from 'src/app/model/document-rh';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DocumentRHService } from 'src/app/services/document-rh.service';
+import { Employe } from 'src/app/model/employe';
+import { EmployeService } from 'src/app/services/employe.service';
 
 
 @Component({
@@ -14,19 +16,27 @@ export class DocumentRHComponent implements OnInit {
   documentrh:DocumentRH;
   lstdocuments:DocumentRH[];
   idEmp:string;
+  idDoc:string;
+  employe:Employe;
+  lstemployes:Employe[];
 
   myForm = new FormGroup({
-    idEmp : new FormControl('',Validators.required),
     idDocument : new FormControl('',Validators.required),
     typeDocument : new FormControl('',Validators.required),
     dateEditionDocument : new FormControl('',Validators.required),
-    employe: new FormControl('',Validators.required)
+    idEmp : new FormControl('',Validators.required)
   });
 
-  constructor(private documentrhservice:DocumentRHService) { this.documentrh = new DocumentRH}
+  myFormDelete = new FormGroup({
+    idDoc : new FormControl('',Validators.required)
+  })
+
+  constructor(private documentrhservice:DocumentRHService, private employeservice:EmployeService)
+    {this.documentrh = new DocumentRH}
 
   ngOnInit() {
     this.getAllDoc();
+    this.getAllEmp();
   }
 
   ajoutDocument(){
@@ -41,6 +51,14 @@ export class DocumentRHComponent implements OnInit {
       this.lstdocuments = data;
     },
       error => {console.log(error); });
+  }
+
+  getAllEmp(){
+    this.employeservice.getAllEmployes().subscribe(data=>{
+      this.lstemployes = data;
+    },
+      error => {console.log(error);
+    });
   }
 
 }
