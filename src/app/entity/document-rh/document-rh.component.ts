5080373b484081a900f8531b9b1bdaf6ebd07b1a
TruongGiang <t.nguyenpham@icloud.com>
2020-02-19 22:11:56 +0100

    document html

--- src/app/entity/document-rh/document-rh.component.ts
+++ src/app/entity/document-rh/document-rh.component.ts
@@ -13,51 +13,67 @@ import { EmployeService } from 'src/app/services/employe.service';
 })
 export class DocumentRHComponent implements OnInit {
 
-  documentrh:DocumentRH;
-  lstdocuments:DocumentRH[];
-  idEmp:string;
-  idDoc:string;
-  employe:Employe;
-  lstemployes:Employe[];
+  documentrh: DocumentRH;
+  lstdocuments: DocumentRH[];
+  idEmp: string;
+  idDoc: string;
+  employe: Employe;
+  lstemployes: Employe[];
 
   myForm = new FormGroup({
-    idDocument : new FormControl('',Validators.required),
-    typeDocument : new FormControl('',Validators.required),
-    dateEditionDocument : new FormControl('',Validators.required),
-    idEmp : new FormControl('',Validators.required)
+    idDocument: new FormControl('', Validators.required),
+    typeDocument: new FormControl('', Validators.required),
+    dateEditionDocument: new FormControl('', Validators.required),
+    idEmp: new FormControl('', Validators.required)
   });
 
   myFormDelete = new FormGroup({
-    idDoc : new FormControl('',Validators.required)
+    idDocument: new FormControl('', Validators.required)
   })
 
-  constructor(private documentrhservice:DocumentRHService, private employeservice:EmployeService)
-    {this.documentrh = new DocumentRH}
+  constructor(private documentrhservice: DocumentRHService, private employeservice: EmployeService) { this.documentrh = new DocumentRH }
 
   ngOnInit() {
     this.getAllDoc();
     this.getAllEmp();
   }
 
-  ajoutDocument(){
-    this.documentrhservice.ajoutDocument({ doc: this.documentrh, idemp: this.idEmp }).subscribe(data=>{
-      console.log( "Document ajouté avec succès !!" );
+  ajoutDocument() {
+    this.documentrhservice.ajoutDocument({ doc: this.documentrh, idemp: this.idEmp }).subscribe(data => {
+      console.log("Document ajouté avec succès !!");
       window.location.reload();
     });
   }
 
-  getAllDoc(){
-    this.documentrhservice.getAllDocuments().subscribe(data=>{
-      this.lstdocuments = data;
+  getDocumentId() {
+    this.documentrhservice.getDocById(this.documentrh.idDocument).subscribe(data => {
+      this.documentrh = data;
+      console.log("Got it !")
     },
-      error => {console.log(error); });
+      error => {
+        console.log(error);
+      });
   }
 
-  getAllEmp(){
-    this.employeservice.getAllEmployes().subscribe(data=>{
+  getAllDoc() {
+    this.documentrhservice.getAllDocuments().subscribe(data => {
+      this.lstdocuments = data;
+    },
+      error => { console.log(error); });
+  }
+
+  getAllEmp() {
+    this.employeservice.getAllEmployes().subscribe(data => {
       this.lstemployes = data;
     },
-      error => {console.log(error);
+      error => {
+        console.log(error);
+      });
+  }
+  supprimerDocument():void{
+    this.documentrhservice.suppDocument(this.documentrh.idDocument).subscribe(data=>{
+      console.log("Document supprimée avec succès !")
+      window.location.reload();
     });
   }
 
