cc4909e9e5777937b2e9d7866860f0e842de1f39
TruongGiang <t.nguyenpham@icloud.com>
2020-02-20 12:00:01 +0100

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { EmployeComponent } from './entity/employe/employe.component';
 import { FormulaireEmpruntComponent } from './entity/formulaire-emprunt/formulaire-emprunt.component';
 import { FournitureComponent } from './entity/fourniture/fourniture.component';
 import { ValidationFournitureComponent } from './validation-fourniture/validation-fourniture.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path:'documentrh', component:DocumentRHComponent},
-  {path:'employe', component:EmployeComponent},
-  {path:'formulaire', component:FormulaireEmpruntComponent},
-  {path:'fourniture', component:FournitureComponent},
-  {path:'validation', component:ValidationFournitureComponent}
+  { path: 'documentrh', component: DocumentRHComponent },
+  { path: 'employe', component: EmployeComponent },
+  { path: 'formulaire', component: FormulaireEmpruntComponent },
+  { path: 'fourniture', component: FournitureComponent },
+  { path: 'validation', component: ValidationFournitureComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
